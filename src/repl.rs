96356270c30096c9cb98f15718dc51e7945b88d5
Pontus Laos <laos.pontus@gmail.com>
2022-10-20 19:33:42 +0200

    Error on invalid command
    And some colors

--- src/repl.rs
+++ src/repl.rs
@@ -1,5 +1,6 @@
+use std::ffi::OsStr;
 use std::io::{self, Write};
-use std::process::Command;
+use std::process;
 
 use crate::Result;
 
@@ -15,34 +16,47 @@ impl Repl {
     }
 
     pub(crate) fn run(&self) -> Result<()> {
-        let mut prev_rc: Option<std::process::ExitStatus> = None;
+        let mut prev_rc: Option<i32> = None;
 
         loop {
             if let Some(exit_code) = prev_rc {
-                if !exit_code.success() {
-                    // FIXME: `.code()` returns `None` if killed by signal
-                    print!("[{}] ", exit_code.code().unwrap());
+                if exit_code != 0 {
+                    print!("\x1b[91m[{}]\x1b[0m ", exit_code);
                 }
             }
-            print!("$ ");
+
+            print!("\x1b[93m$\x1b[0m ");
             io::stdout().flush()?;
 
-            let (cmd, args) = read_input()?;
+            let (command, args) = read_input()?;
 
-            if self.available_cmds.contains(&cmd) {
-                let mut child = Command::new(cmd).args(args).spawn().unwrap();
-                let code = child.wait().unwrap();
-                prev_rc = Some(code);
-            } else {
-                if cmd == "exit" {
-                    std::process::exit(0);
+            match command.as_str() {
+                "" => {}
+
+                "exit" => process::exit(0),
+
+                cmd if self.available_cmds.iter().any(|s| s == cmd) => {
+                    let result = run_cmd(command, &args)?;
+                    prev_rc = Some(result);
                 }
-                println!("{cmd} {}", args.join(" "));
+
+                cmd => {
+                    println!("Unknown command: {cmd}");
+                    prev_rc = Some(1);
+                }
             }
 
             io::stdout().flush()?;
         }
     }
+}
+
+fn run_cmd<S: AsRef<OsStr>>(cmd: S, args: &[S]) -> Result<i32> {
+    let mut child = process::Command::new(cmd).args(args).spawn().unwrap();
+    let result = child.wait()?;
+
+    // FIXME: `.code()` returns `None` if killed by signal
+    Ok(result.code().unwrap())
 }
 
 fn get_cmds_from_path() -> Vec<String> {
@@ -52,10 +66,10 @@ fn get_cmds_from_path() -> Vec<String> {
     let mut cmds = Vec::new();
 
     for path in raw_path {
-        let output = Command::new("ls").arg(path).output().unwrap();
+        let output = process::Command::new("ls").arg(path).output().unwrap();
 
         let s = String::from_utf8(output.stdout).unwrap();
-        let s = s.split_ascii_whitespace().map(|s| s.to_string()).collect::<Vec<_>>();
+        let s = s.split_ascii_whitespace().map(ToString::to_string);
         cmds.extend(s);
     }
 
@@ -67,13 +81,14 @@ fn read_input() -> Result<(String, Vec<String>)> {
     io::stdin().read_line(&mut buffer)?;
     let buffer = buffer.trim().to_string();
 
-    // println!("Buffer: '{buffer}'");
-
     if buffer.find(' ').is_some() {
         let (cmd, args) = buffer.split_once(' ').unwrap();
-        let args = args.split_ascii_whitespace().map(ToString::to_string).collect::<Vec<_>>();
+        let args = args
+            .split_ascii_whitespace()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>();
         return Ok((cmd.to_string(), args));
     }
 
-    Ok((buffer.to_string(), Vec::new()))
+    Ok((buffer, Default::default()))
 }
