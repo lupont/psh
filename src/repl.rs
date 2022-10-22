ca53d595037d5d4b11b56dce0a0aef2bdeecf24a
Pontus Laos <laos.pontus@gmail.com>
2022-10-22 11:55:59 +0200

    Implement `cd` builtin
    Show current dir in prompt, allow argumentless `cd` to go home.
    
    Also fix some clippy complaints

--- src/repl.rs
+++ src/repl.rs
@@ -15,7 +15,10 @@ impl Repl {
     pub(crate) fn init() -> Self {
         Self {
             available_cmds: get_cmds_from_path(),
-            builtins: vec!["debug".to_string()],
+            builtins: vec!["debug", "cd", "exit"]
+                .iter()
+                .map(ToString::to_string)
+                .collect(),
         }
     }
 
@@ -29,10 +32,28 @@ impl Repl {
         self.builtins.iter().any(|s| s == cmd.as_ref())
     }
 
-    fn execute_builtin(&self, cmd: impl AsRef<str>) -> i32 {
+    fn execute_builtin(&self, cmd: impl AsRef<str>, args: &[impl AsRef<str>]) -> i32 {
         match cmd.as_ref() {
+            "exit" => process::exit(0),
+
             "debug" => {
                 println!("{:#?}", self);
+                0
+            }
+
+            "cd" => {
+                let path = if args.is_empty() {
+                    PathBuf::from(home_dir().unwrap())
+                } else {
+                    let path = PathBuf::from(args[0].as_ref());
+                    if path.exists() {
+                        path
+                    } else {
+                        println!("cd: The path '{}' does not exist", args[0].as_ref());
+                        return 1;
+                    }
+                };
+                std::env::set_current_dir(path).unwrap();
                 0
             }
 
@@ -47,6 +68,7 @@ impl Repl {
         let mut prev_rc: Option<i32> = None;
 
         loop {
+            print!("{} ", std::env::current_dir().unwrap().display().to_string().replace(&home_dir()?, "~"));
             match prev_rc {
                 Some(code) if code != 0 => {
                     print!("\x1b[91m[{}]\x1b[0m ", code);
@@ -62,10 +84,8 @@ impl Repl {
             match command.as_str() {
                 "" => {}
 
-                "exit" => process::exit(0),
-
                 cmd if self.has_builtin(cmd) => {
-                    prev_rc = Some(self.execute_builtin(cmd));
+                    prev_rc = Some(self.execute_builtin(cmd, &args));
                 }
 
                 cmd if self.has_cmd(cmd) => {
@@ -84,11 +104,14 @@ impl Repl {
 }
 
 fn run_cmd(cmd: impl AsRef<OsStr>, args: &[impl AsRef<OsStr>]) -> Result<i32> {
-    let mut child = process::Command::new(cmd).args(args).spawn().unwrap();
-    let result = child.wait()?;
+    let child = process::Command::new(cmd).args(args).spawn().unwrap();
+    let result = child.wait_with_output()?;
+
+    // FIXME: append new line if child did not print one?
+    // println!("stdout: '{}'", String::from_utf8_lossy(&result.stdout));
 
     // FIXME: `.code()` returns `None` if killed by signal
-    Ok(result.code().unwrap())
+    Ok(result.status.code().unwrap())
 }
 
 fn get_cmds_from_path() -> Vec<String> {
@@ -98,13 +121,16 @@ fn get_cmds_from_path() -> Vec<String> {
     let mut cmds = Vec::new();
 
     for path in raw_path {
-        match std::fs::read_dir(path) {
-            Ok(dirs) => cmds.extend(dirs.map(|d| format!("{}", d.unwrap().path().display()))),
-            _ => {}
+        if let Ok(dirs) = std::fs::read_dir(path) {
+            cmds.extend(dirs.map(|d| format!("{}", d.unwrap().path().display())));
         }
     }
 
     cmds
+}
+
+fn home_dir() -> Result<String> {
+    std::env::var("HOME").map_err(|_| Error::NoHome)
 }
 
 /// Reads input from the user and saves it to the history file.
@@ -113,10 +139,7 @@ fn read_input_and_save_history() -> Result<(String, Vec<String>)> {
     io::stdin().read_line(&mut buffer)?;
     let buffer = buffer.trim().to_string();
 
-    let home = match std::env::var("HOME") {
-        Ok(home) => home,
-        Err(_) => return Err(Error::NoHome),
-    };
+    let home = home_dir()?;
 
     if !buffer.is_empty() {
         let history_file = match std::env::var("RUSH_HISTFILE") {
@@ -139,7 +162,7 @@ fn read_input_and_save_history() -> Result<(String, Vec<String>)> {
             let args = args
                 .split_ascii_whitespace()
                 .map(ToString::to_string)
-                .map(|s| s.replace("~", &home))
+                .map(|s| s.replace('~', &home))
                 .collect::<Vec<_>>();
             Ok((cmd.to_string(), args))
         }
