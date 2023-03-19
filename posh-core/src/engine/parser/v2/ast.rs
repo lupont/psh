0f4a6e9759590d309674f0066a10d42434676f6d
Pontus Laos <laos.pontus@gmail.com>
2023-03-19 11:14:07 +0100

    feat: First version of parse_simple_command()
    Seems to work unexpectedly well! I'm really liking the direction of the
    new parser.

--- posh-core/src/engine/parser/v2/ast.rs
+++ posh-core/src/engine/parser/v2/ast.rs
@@ -13,6 +13,8 @@ pub trait Parser: Iterator<Item = SemanticToken> {
     fn parse_redirection(&mut self) -> Option<Redirection>;
     fn parse_word(&mut self) -> Option<Word>;
 
+    fn parse_simple_command(&mut self) -> Option<SimpleCommand>;
+
     fn swallow_whitespace(&mut self);
 
     fn parse(&mut self) -> Option<SyntaxTree>;
@@ -26,6 +28,70 @@ where
         todo!()
     }
 
+    fn parse_simple_command(&mut self) -> Option<SimpleCommand> {
+        let initial = self.clone();
+
+        let mut redirections = Vec::new();
+        let mut variable_assignments = Vec::new();
+        let mut words = Vec::<Word>::new();
+
+        enum Valid {
+            R(Redirection),
+            V(VariableAssignment),
+        }
+
+        while let Some(thing) = self
+            .parse_variable_assignment()
+            .map(Valid::V)
+            .or_else(|| self.parse_redirection().map(Valid::R))
+        {
+            match thing {
+                Valid::R(r) => redirections.push(r),
+                Valid::V(v) => variable_assignments.push(v),
+            }
+            self.swallow_whitespace();
+        }
+
+        self.swallow_whitespace();
+
+        match self.parse_word() {
+            None => {
+                *self = initial;
+                None
+            }
+
+            Some(word) => {
+                let name = word;
+
+                enum Valid {
+                    R(Redirection),
+                    W(Word),
+                }
+
+                self.swallow_whitespace();
+
+                while let Some(thing) = self
+                    .parse_redirection()
+                    .map(Valid::R)
+                    .or_else(|| self.parse_word().map(Valid::W))
+                {
+                    match thing {
+                        Valid::R(redirection) => redirections.push(redirection),
+                        Valid::W(word) => words.push(word),
+                    }
+                    self.swallow_whitespace();
+                }
+
+                Some(SimpleCommand {
+                    redirections,
+                    assignments: variable_assignments,
+                    name,
+                    words,
+                })
+            }
+        }
+    }
+
     fn swallow_whitespace(&mut self) {
         while let Some(SemanticToken::Whitespace(_)) = self.peek() {
             self.next();
@@ -35,81 +101,80 @@ where
     fn parse_redirection(&mut self) -> Option<Redirection> {
         let initial = self.clone();
 
-        let fd = self
-            .consume_if(|t| matches!(t, SemanticToken::Word(_)))
-            .map(|t| match t {
-                SemanticToken::Word(s) => Word::new(&s),
-                _ => unreachable!(),
-            })
-            .unwrap_or_else(|| Word::new("0"));
+        let fd = match self.peek() {
+            Some(SemanticToken::Word(s)) => {
+                let word = Word::new(s);
+                self.next();
+                word
+            }
+            _ => Word::new("0"),
+        };
 
-        if let Some(token) = self
+        match self
             .consume_single(SemanticToken::RedirectOutput)
             .or_else(|| self.consume_single(SemanticToken::RedirectInput))
         {
-            match token {
-                SemanticToken::RedirectInput => {
-                    let is_here_doc = self.consume_single(SemanticToken::RedirectInput).is_some();
+            Some(SemanticToken::RedirectInput) => {
+                let is_here_doc = self.consume_single(SemanticToken::RedirectInput).is_some();
 
-                    self.swallow_whitespace();
+                self.swallow_whitespace();
 
-                    match self.peek() {
-                        Some(SemanticToken::Word(s)) => {
-                            let target = Word::new(&s);
-                            self.next();
-                            Some(if is_here_doc {
-                                Redirection::HereDocument {
-                                    file_descriptor: fd,
-                                    delimiter: target,
-                                }
-                            } else {
-                                Redirection::Input {
-                                    file_descriptor: fd,
-                                    target,
-                                }
-                            })
-                        }
+                match self.peek() {
+                    Some(SemanticToken::Word(s)) => {
+                        let target = Word::new(s);
+                        self.next();
+                        Some(if is_here_doc {
+                            Redirection::HereDocument {
+                                file_descriptor: fd,
+                                delimiter: target,
+                            }
+                        } else {
+                            Redirection::Input {
+                                file_descriptor: fd,
+                                target,
+                            }
+                        })
+                    }
 
-                        _ => {
-                            *self = initial;
-                            return None;
-                        }
+                    _ => {
+                        *self = initial;
+                        None
                     }
                 }
-
-                SemanticToken::RedirectOutput => {
-                    let append = self.consume_single(SemanticToken::RedirectOutput).is_some();
-
-                    self.swallow_whitespace();
-
-                    let target = match self.consume_if(|t| matches!(t, SemanticToken::Word(_))) {
-                        Some(SemanticToken::Word(s)) => s,
-                        Some(_) => unreachable!(),
-                        None => {
-                            *self = initial;
-                            return None;
-                        }
-                    };
-
-                    Some(Redirection::Output {
-                        file_descriptor: fd,
-                        append,
-                        target: Word::new(&target),
-                    })
-                }
-
-                _ => unreachable!(),
             }
-        } else {
-            *self = initial;
-            None
+
+            Some(SemanticToken::RedirectOutput) => {
+                let append = self.consume_single(SemanticToken::RedirectOutput).is_some();
+
+                self.swallow_whitespace();
+
+                let target = match self.consume_if(|t| matches!(t, SemanticToken::Word(_))) {
+                    Some(SemanticToken::Word(s)) => s,
+                    Some(_) => unreachable!(),
+                    None => {
+                        *self = initial;
+                        return None;
+                    }
+                };
+
+                Some(Redirection::Output {
+                    file_descriptor: fd,
+                    append,
+                    target: Word::new(&target),
+                })
+            }
+
+            _ => {
+                *self = initial;
+                None
+            }
         }
     }
 
     fn parse_word(&mut self) -> Option<Word> {
         match self.peek() {
             Some(SemanticToken::Word(s)) => {
-                let word = Word::new(&s);
+                let word = Word::new(s);
                 self.next();
                 Some(word)
             }
@@ -192,11 +257,23 @@ pub struct Word {
 
 impl Word {
     pub fn new(input: &str) -> Self {
+        let input = Self::do_quote_removal(input);
+        let input = Self::expand(input);
+
         Self {
             name: input.to_string(),
-            // FIXME: implement expansion recognition
             expansions: Default::default(),
         }
+    }
+
+    fn do_quote_removal(input: &str) -> &str {
+        // TODO: do quote removal
+        input
+    }
+
+    fn expand(input: &str) -> &str {
+        // TODO: expand
+        input
     }
 }
 
@@ -339,6 +416,17 @@ mod tests {
             assert_eq!(Some(expected), actual);
         }
 
+        for item in vec!["2>>'foo bar baz'", "2>> 'foo bar baz'"] {
+            let mut tokens = parse(item);
+            let actual = tokens.parse_redirection();
+            let expected = Redirection::Output {
+                file_descriptor: Word::new("2"),
+                append: true,
+                target: Word::new("'foo bar baz'"),
+            };
+            assert_eq!(Some(expected), actual);
+        }
+
         let mut tokens = parse(">><");
         let actual = tokens.parse_redirection();
         assert!(actual.is_none());
@@ -369,5 +457,54 @@ mod tests {
             };
             assert_eq!(Some(expected), actual);
         }
+    }
+
+    fn output_redirection(fd: &str, append: bool, target: &str) -> Redirection {
+        Redirection::Output {
+            file_descriptor: Word::new(fd),
+            append,
+            target: Word::new(target),
+        }
+    }
+
+    fn input_redirection(fd: &str, target: &str) -> Redirection {
+        Redirection::Input {
+            file_descriptor: Word::new(fd),
+            target: Word::new(target),
+        }
+    }
+
+    fn here_doc(fd: &str, delimiter: &str) -> Redirection {
+        Redirection::HereDocument {
+            file_descriptor: Word::new(fd),
+            delimiter: Word::new(delimiter),
+        }
+    }
+
+    #[test]
+    fn parse_simple_command() {
+        let mut tokens = parse("foo='bar baz' 3>foo bar=yo echo 4</dev/null foo 2>> stderr.log bar baz");
+        let actual = tokens.parse_simple_command();
+
+        let expected = SimpleCommand {
+            redirections: vec![
+                output_redirection("3", false, "foo"),
+                input_redirection("4", "/dev/null"),
+                output_redirection("2", true, "stderr.log"),
+            ],
+            assignments: vec![
+                VariableAssignment::new("foo", Some("'bar baz'")),
+                VariableAssignment::new("bar", Some("yo")),
+            ],
+            name: Word::new("echo"),
+            words: vec![
+                Word::new("foo"),
+                Word::new("bar"),
+                Word::new("baz"),
+            ],
+        };
+
+        assert_eq!(Some(expected), actual);
+        assert!(tokens.next().is_none());
     }
 }
