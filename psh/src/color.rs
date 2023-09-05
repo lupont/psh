d466dc550ae925e3a314779241cac2ee769dfdc0
Pontus Laos <laos.pontus@gmail.com>
2023-09-05 16:47:19 +0200

    misc: Use declarative macro for color functions
    There was lots of repetition in the color functions, with the only
    differing things being the function name and the name of the environment
    variable to read. Perfect use case for a little macro, I say :-)

--- psh/src/color.rs
+++ psh/src/color.rs
@@ -1,56 +1,27 @@
 use crossterm::style::Color;
 use psh_core::Engine;
 
-fn from_var(var: &str, engine: &Engine) -> Color {
-    match engine.get_value_of(var) {
-        Some(color) => match color.parse::<u8>() {
-            Ok(val) => Color::AnsiValue(val),
-            Err(_) => Color::AnsiValue(15),
-        },
-        None => Color::AnsiValue(15),
-    }
+macro_rules! color {
+    ($func:ident, $var:expr) => {
+        pub fn $func(engine: &Engine) -> Color {
+            Color::AnsiValue(
+                engine
+                    .get_value_of($var)
+                    .and_then(|c| c.parse::<u8>().ok())
+                    .unwrap_or(15),
+            )
+        }
+    };
 }
 
-pub fn unparsed(engine: &Engine) -> Color {
-    from_var("PSH_UNPARSED_COL", engine)
-}
-
-pub fn comment(engine: &Engine) -> Color {
-    from_var("PSH_COMMENT_COL", engine)
-}
-
-pub fn separator(engine: &Engine) -> Color {
-    from_var("PSH_SEPARATOR_COL", engine)
-}
-
-pub fn valid_cmd(engine: &Engine) -> Color {
-    from_var("PSH_VALID_CMD_COL", engine)
-}
-
-pub fn invalid_cmd(engine: &Engine) -> Color {
-    from_var("PSH_INVALID_CMD_COL", engine)
-}
-
-pub fn cmd_sub(engine: &Engine) -> Color {
-    from_var("PSH_CMD_SUB_COL", engine)
-}
-
-pub fn normal(engine: &Engine) -> Color {
-    from_var("PSH_NORMAL_COL", engine)
-}
-
-pub fn op(engine: &Engine) -> Color {
-    from_var("PSH_OP_COL", engine)
-}
-
-pub fn lhs(engine: &Engine) -> Color {
-    from_var("PSH_LHS_COL", engine)
-}
-
-pub fn rhs(engine: &Engine) -> Color {
-    from_var("PSH_RHS_COL", engine)
-}
-
-pub fn prompt(engine: &Engine) -> Color {
-    from_var("PSH_PROMPT_COL", engine)
-}
+color!(cmd_sub, "PSH_CMD_SUB_COL");
+color!(comment, "PSH_COMMENT_COL");
+color!(invalid_cmd, "PSH_INVALID_CMD_COL");
+color!(lhs, "PSH_LHS_COL");
+color!(normal, "PSH_NORMAL_COL");
+color!(op, "PSH_OP_COL");
+color!(prompt, "PSH_PROMPT_COL");
+color!(rhs, "PSH_RHS_COL");
+color!(separator, "PSH_SEPARATOR_COL");
+color!(unparsed, "PSH_UNPARSED_COL");
+color!(valid_cmd, "PSH_VALID_CMD_COL");
